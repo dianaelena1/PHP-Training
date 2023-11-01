f1c0db1f04f3456e5444281962a938ea54384962
Diana Elena <diana.elenaa1@gmail.com>
2023-11-01 16:55:21 +0200

    Delete Tema3/ex3.php

--- Tema3/ex3.php
+++
@@ -1,4 +0,0 @@
-<!-- Luati datele din formular , 
-verificati daca parola este corecta, si daca este corecta userul sa fie directionat catre pagina logat.php-->
-<!-- in logat php, doar afisati un un html basic , de preferat o imaginea cu un brackground   -->
-
